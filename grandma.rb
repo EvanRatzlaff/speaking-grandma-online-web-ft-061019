ce2322f0bf98c20a335afad2c1fa824fe5446e52
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-13 21:52:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if (phrase == to_all_caps)
+  if (phrase == to.upcase)
     puts "NO, NOT SINCE 1938!"
   else 
     puts "HUH?! SPEAK UP, SONNY!"
+  elsif 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
